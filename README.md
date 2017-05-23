3584bd8fc47a51f06045a661522aa8339ff6fb34
Mike Gozzo <mike@smooch.io>
2017-05-23 19:18:14 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# smooch-latency
+A latency tester for Smooch User Message Webhooks
+
+1. Sign up for smooch and get an API key/secret. Drop these in a .env file in the code directory with variables named SMOOCH_KEY, SMOOCH_SECRET
+2. Create a webhook on app user messages and point it at an ngrok url
+3. Run this script
---
+++ smooch-latency.js
@@ -0,0 +1,64 @@
+const smoochCore = require('smooch-core');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const Guid = require('guid');
+
+require('dotenv').config()
+
+var queue = [];
+var counter = 0;
+var appUserId = '';
+
+function sendMessage() {
+  queue[counter] = Date.now();
+
+  smooch.appUsers.sendMessage(appUserId, {
+    text: counter.toString(),
+    role: 'appUser',
+    type: 'text'
+  });
+
+  counter++
+}
+
+app.use(bodyParser.json());
+
+app.post('/hook', function (req, res) {
+  if(req.body.appUser._id == appUserId) {
+    var ts = Date.now();
+    var idx = req.body.messages[0].text;
+    var delta = ts - queue[idx];
+    console.log("Received message " + idx + " - Webhook Latency: " + delta + "ms");
+
+    setTimeout(sendMessage, 1000);
+  } else {
+    console.log("INFO - Received a webhook for user " + req.body.appUser._id + ", but we're tracking " + appUserId);
+  }
+
+  res.sendStatus(200);
+})
+
+app.listen(3000, function () {
+  console.log('Latency tester listening on port 3000!')
+})
+
+var smooch = new smoochCore({
+    keyId: process.env.SMOOCH_KEY,
+    secret: process.env.SMOOCH_SECRET,
+    scope: 'app'
+});
+
+var deviceId = Guid.raw();
+
+//Initialize the device
+smooch.appUsers.init({
+    device: {
+        id: deviceId,
+        platform: 'other',
+        appVersion: '1.0'
+    }
+}).then((response) => {
+    appUserId = response.appUser._id;
+    sendMessage(appUserId);
+});
