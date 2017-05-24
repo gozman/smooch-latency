07360e1312bf69e8736299fe2742bc504322c788
Mike Gozzo <mike@smooch.io>
2017-05-24 04:23:51 -0400

    average latency calc

--- smooch-latency.js
+++ smooch-latency.js
@@ -11,7 +11,11 @@ var counter = 0;
 var appUserId = '';
 
 function sendMessage() {
-  queue[counter] = Date.now();
+  var obj = {
+    start: Date.now()
+  }
+
+  queue[counter] = obj;
 
   smooch.appUsers.sendMessage(appUserId, {
     text: counter.toString(),
@@ -25,12 +29,14 @@ function sendMessage() {
 app.use(bodyParser.json());
 
 app.post('/hook', function (req, res) {
-  if(req.body.appUser._id == appUserId) && req.body.trigger == "message:appUser") {
-    var ts = Date.now();
+  if(req.body.appUser._id == appUserId && req.body.trigger == "message:appUser") {
     var idx = req.body.messages[0].text;
-    var delta = ts - queue[idx];
-    console.log("Received message " + idx + " - Webhook Latency: " + delta + "ms");
+    var obj = queue[idx];
+    obj.end = Date.now();
+    obj.delta = obj.end - obj.start;
+    queue[idx] = obj;
 
+    console.log("Received message " + idx + " - Webhook Latency: " + obj.delta + "ms");
     setTimeout(sendMessage, 1000);
   } else if (req.body.trigger == "message:appUser") {
     console.log("INFO - Ignoring a received a webhook for user " + req.body.appUser._id + ", but we're tracking " + appUserId);
@@ -62,5 +68,26 @@ smooch.appUsers.init({
     }
 }).then((response) => {
     appUserId = response.appUser._id;
-    sendMessage(appUserId);
+    console.log(" Testing latency using AppUser with ID: " + appUserId + "\n");
+    console.log("-------------------------------------------------------------------------------");
+    sendMessage();
+});
+
+process.on('SIGINT', function() {
+    console.log("\n Caught interrupt signal \n");
+
+    var accum = 0;
+
+    for(var i=0; i<queue.length; i++) {
+      accum += queue[i].delta;
+    }
+
+    if(queue.length) {
+      console.log("-------------------------------------------------------------------------------\n");
+      console.log(" Number of messages sent: " + queue.length);
+      console.log(" Average latency: " + accum/queue.length + "ms\n");
+      console.log("-------------------------------------------------------------------------------\n");
+    }
+
+    process.exit();
 });
